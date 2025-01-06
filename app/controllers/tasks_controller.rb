db66bc057cfe61e09d818a91139453e491694da9
Your Name <you@example.com>
2025-01-06 11:58:03 +0900

    add: タスク管理画面2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,7 +6,6 @@ class TasksController < ApplicationController
   def new
     @dates = (1..60).map { |i| Date.today + i }
     @mon = AvailableTime.where("weekday LIKE ?", "%Mon%").where("available_time_slot LIKE ?", "%12:00%")
-    
   end
 
   def create
@@ -16,7 +15,7 @@ end
 private
 
   def find_clinics_by_weekday
-    today_weekday = Date.today.strftime('%a')
+    today_weekday = Date.today.strftime("%a")
     clinics = Clinic.joins(:available_times)
                   .where(available_times: { weekday: "Mon" })
                   .distinct
