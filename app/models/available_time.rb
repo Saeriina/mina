d5c88dfd5c4258175719cbb17d2aaec7b2144548
Your Name <you@example.com>
2025-01-25 10:32:21 +0900

    add: スケジュール表示・削除機能

--- app/models/available_time.rb
+++ app/models/available_time.rb
@@ -1,29 +1,9 @@
 class AvailableTime < ApplicationRecord
-  validate :validate_weekdays
-  validate :validate_times
-
+  # アソシエーション
   belongs_to :clinic
+  belongs_to :user
 
-
-  private
-
-  def validate_weekdays
-    valid_days = %w[ Mon Tuesday Wednesday Thursday Friday ]
-    visitable_days = weekday.is_a?(String) ? JSON.parse(weekday) : weekday
-
-    invalid_days = visitable_days - valid_days
-    if invalid_days.any?
-      errors.add(:weekday, "に無効な曜日が含まれています: #{invalid_days.join(', ')}")
-    end
-  end
-
-  def validate_times
-    valid_times = %w[ 12:00 12:30 13:00 13:30 14:00 14:30 15:00 15:30 16:00 17:00 18:00 ]
-    visitable_times = available_time_slot.is_a?(String) ? JSON.parse(available_time_slot) : available_time_slot
-
-    invalid_times = visitable_times - valid_times
-    if invalid_times.any?
-      errors.add(:available_time_slot, "に無効な時間が含まれています: #{invalid_times.join(', ')}")
-    end
-  end
+  # バリデーション
+  validates :weekday, presence: true
+  validates :available_time_slot, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   post "login", to: "user_sessions#create"
   delete "logout", to: "user_sessions#destroy"
   resources :products, only: %i[index new create edit destroy update]
-  resources :tasks, only: %i[index new create]
+  resources :tasks, only: %i[index new create destroy]
 end
