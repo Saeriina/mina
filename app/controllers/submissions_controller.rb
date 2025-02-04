55e54ae3c3ad828c2896ea684b5172b49e81be87
Your Name <you@example.com>
2025-02-04 20:53:55 +0900

    add: 提出物作成機能2

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -6,7 +6,7 @@ class SubmissionsController < ApplicationController
   def create
     @submission = current_user.submissions.build(submission_params)
     if @submission.save
-      redirect_to tasks_path, success: "#{Submission.model_name.human}が作成されました。"           
+      redirect_to tasks_path, success: "#{Submission.model_name.human}が作成されました。"
     else
       flash.now[:danger] = "#{Submission.model_name.human}の作成に失敗しました。"
       render :new, status: :unprocessable_entity
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -20,7 +20,7 @@ class TasksController < ApplicationController
     @times = [ "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "17:00", "18:00" ]
 
     # 提出物のデータを取得
-		@submissions = Submission.includes(:user)
+    @submissions = Submission.includes(:user)
   end
 
   def new
