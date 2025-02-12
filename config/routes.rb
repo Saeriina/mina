ad0deff410054857a55b953270254ff596e68fe6
Your Name <you@example.com>
2025-02-12 20:28:13 +0900

    add: パスワードリセット機能3

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
       get :auto_schedule
     end
   end
-  resources :password_resets, only: [:create, :edit, :update, :new]
+  resources :password_resets, only: [ :create, :edit, :update, :new ]
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
