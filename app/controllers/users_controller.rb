17126f2bc35a3f7c4b3c8dd74fa18c66d90964cd
Your Name <you@example.com>
2025-06-26 18:27:05 +0900

    fix: フラッシュメッセージ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
       redirect_to root_path, success: "ユーザーを登録しました"
     else
       flash.now[:danger] = "ユーザー登録に失敗しました"
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
